712d8667a2e2efbe24b29efa555505c01b6327c0
kevinsshaffer <kevinsshaffer@gmail.com>
2021-03-29 23:22:12 -0600

    Add First File

---
+++ sockets.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    WSADATA wsa;
+
+    printf("\nInitialising Winsock...");
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("Failed.  Error Code : %d", WSAGetLastError());
+        return 1;
+    }
+
+    printf("Initialised.");
+
+    return 0;
+}
